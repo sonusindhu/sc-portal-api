bfc32f9183937b25ad5ed987536aaf0d18ab1699
Sonu Sindhu <sonusindhu10@yahoo.com>
2022-06-21 20:58:52 +0530

    added note model

---
+++ api/models/Note.js
@@ -0,0 +1,50 @@
+/**
+ * Stop.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+  tableName: "stops",
+  migrate: "alter",
+
+  attributes: {
+    type: {
+      type: "string",
+      maxLength: 10,
+    },
+    title: {
+      type: "string",
+      maxLength: 100,
+    },
+    message: {
+      type: "string",
+      maxLength: 5000,
+    },
+    markCritical: {
+      type: "boolean",
+      defaultsTo: false,
+      allowNull: true,
+    },
+    quoteId: {
+      model: "quote",
+      allowNull: true,
+    },
+    contactId: {
+      model: "contact",
+      allowNull: true,
+    },
+    companyId: {
+      model: "company",
+      allowNull: true,
+    },
+    inventoryId: {
+      model: "inventory",
+      allowNull: true,
+    },
+    userId: {
+      model: "user",
+    },
+  },
+};
