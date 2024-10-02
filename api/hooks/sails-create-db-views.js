74cbcca42bf2a4a048eccf8368092f4277fb3c9c
Sonu Sindhu <sonusindhu10@yahoo.com>
2024-10-02 18:38:42 +0530

    minor fix

--- api/hooks/sails-create-db-views.js
+++ api/hooks/sails-create-db-views.js
@@ -13,7 +13,7 @@ module.exports = function (sails) {
       sails.after("hook:orm:loaded", async function () {
         const company_view = `CREATE OR REPLACE VIEW view_company_list as select c.*, uc.fullName AS createdByName,up.fullName AS updatedByName, cn.fullName as mainContactName from companies as c left join users uc on uc.id = c.createdBy left join sails_scpapi.users up on uc.id = c.updatedBy left join sails_scpapi.contacts cn on cn.id = c.mainContact GROUP BY c.id;`;
 
-        const contact_view = `CREATE OR REPLACE VIEW view_contact_list as select cn.*, uc.fullName AS createdByName,up.fullName AS updatedByName, c.name as companyName from contacts as cn left join users uc on uc.id = cn.createdBy left join sails_scpapi.users up on uc.id = cn.updatedBy left join sails_scpapi.companies c on c.id = cn.companyId GROUP BY uc.id;`;
+        const contact_view = `CREATE OR REPLACE VIEW view_contact_list as select cn.*, uc.fullName AS createdByName,up.fullName AS updatedByName, c.name as companyName from contacts as cn left join users uc on uc.id = cn.createdBy left join sails_scpapi.users up on uc.id = cn.updatedBy left join sails_scpapi.companies c on c.id = cn.companyId GROUP BY cn.companyId;`;
 
         const inventory_view = `CREATE OR REPLACE VIEW view_inventory_list as select i.*, uc.fullName AS createdByName, up.fullName AS updatedByName, c.name AS companyName from inventories as i left join companies as c on c.id = i.company left join users as uc on uc.id = i.createdBy left join users as up on up.id = i.updatedBy GROUP BY up.id;`;
 
